eed47eb23926af5730036398bb671cf5eb27ff45
Thibault <76521322+thibault0712@users.noreply.github.com>
2024-07-21 18:31:43 +0200

    Update renderer.js to avoid a start if requirement system isn't good

--- src/loader/renderer.js
+++ src/loader/renderer.js
@@ -8,38 +8,36 @@ loadingAction.textContent = "Vérification de la configuration"
 ipcRenderer.invoke("systemRequirement").then((result) => {
   if (result === "ok"){
     loadingAction.textContent = "Configuration vérifié"
+    loadingAction.textContent = "Recherche de mise à jour"
+    ipcRenderer.send("updateVerification");
+    
+    ipcRenderer.on("updateAvailable", () => {
+      loadingAction.textContent = "Mise à jour disponible"
+    })
+    
+    ipcRenderer.on("updateNotAvailable", () => {
+      loadingAction.textContent = "Pas de mise à jour"
+      ipcRenderer.send("loadLauncher");
+    })
+    
+    ipcRenderer.on("updatePercentageValue", (event, percentage) => {
+      loadingAction.textContent = "Mise à jour"
+      const _percentage = percentage + "%"
+      loadingBar.style.width = _percentage;
+      loadingPercentage.classList.remove("invisible");
+      loadingPercentage.textContent = _percentage
+    })
+    
+    ipcRenderer.on("updateDone", () => {
+      loadingAction.textContent = "Mise à jour terminé !"
+      ipcRenderer.send("loadLauncher");
+    })
+    
+    ipcRenderer.on("skipUpdate", () => {
+      loadingAction.textContent = "Mode développement détecté mise à jour évité"
+      ipcRenderer.send("loadLauncher")
+    })
   }else{
     loadingAction.textContent = "Problème de configuration logiciel"
   }
 })
-
-loadingAction.textContent = "Recherche de mise à jour"
-ipcRenderer.send("updateVerification");
-
-ipcRenderer.on("updateAvailable", () => {
-  loadingAction.textContent = "Mise à jour disponible"
-})
-
-ipcRenderer.on("updateNotAvailable", () => {
-  loadingAction.textContent = "Pas de mise à jour"
-  ipcRenderer.send("loadLauncher");
-})
-
-ipcRenderer.on("updatePercentageValue", (event, percentage) => {
-  loadingAction.textContent = "Mise à jour"
-  const _percentage = percentage + "%"
-  loadingBar.style.width = _percentage;
-  loadingPercentage.classList.remove("invisible");
-  loadingPercentage.textContent = _percentage
-})
-
-ipcRenderer.on("updateDone", () => {
-  loadingAction.textContent = "Mise à jour terminé !"
-  ipcRenderer.send("loadLauncher");
-})
-
-ipcRenderer.on("skipUpdate", () => {
-  loadingAction.textContent = "Mode développement détecté mise à jour évité"
-  ipcRenderer.send("loadLauncher")
-})
-
